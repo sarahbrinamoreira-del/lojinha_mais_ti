712f1505af6b7c4e960dc0448577e24ca387b5ac
sarahbrinamoreira-del <sarahbrinamoreira@gmail.com>
2026-02-04 11:21:11 -0300

    Update database.js

--- database.js
+++ database.js
@@ -1,1 +1,23 @@
+const Database = require ('better-sqlite3');
 
+class ConexaoBanco {
+
+
+
+    this._criarTabela ();
+}
+
+_criarTabela (){
+    const sql = 
+
+`CREATE TABLE PRODUTOS (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+nome TXT NOT NULL,
+valor REAL NOT NULL,
+qtd INTEGER NOT NULL
+)`
+
+    this.db.prepare(sql).run();
+}
+
+module.exports = new ConexaoBanco().db;
